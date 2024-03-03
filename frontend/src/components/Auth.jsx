0d27b323e91426ae490fb443c5c4e352b9a89d78
Jazmin Durand <50839980+durand-jazmin@users.noreply.github.com>
2024-03-03 07:54:34 +0100

    Update Auth.jsx

--- frontend/src/components/Auth.jsx
+++ frontend/src/components/Auth.jsx
@@ -5,12 +5,6 @@ import { Navigate } from "react-router-dom";
 
 const Auth = () => {
   const { user } = useContext(AuthContext);
-<<<<<<< HEAD
- 
-=======
-
->>>>>>> 22268522bbb5f61fd1c1eb07596ca8cc2bc48da5
-
   return user ? (
     <div>
       <HomePage />
