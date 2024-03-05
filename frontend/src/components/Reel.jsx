81b472d77b73e93c2be2073630f7e44247b3c5c9
Jazmin Durand <50839980+durand-jazmin@users.noreply.github.com>
2024-03-05 16:45:56 +0100

    Update search y like component in Reel.jsx

--- frontend/src/components/Reel.jsx
+++ frontend/src/components/Reel.jsx
@@ -2,16 +2,15 @@ import { Link, useNavigate } from "react-router-dom";
 import React, { useContext, useState } from "react";
 import { deleteReelService } from "../services";
 import { AuthContext } from "../context/AuthContext";
-import { LikeButton, LikesComponent } from "./LikesComponent";
+import { LikeComponent } from "./LikeComponent";
 import { ConfirmationPopup } from "./ConfirmationPopup";
 import "./Reel.css";
 
-
 const base_URL = "http://localhost:3000";
 
-const Reel = ({ user, reel, removeReel }) => {
+const Reel = ({ user, reel, removeReel, searchTerm}) => {
   const navigate = useNavigate();
-  const { token, like, toggleLike } = useContext(AuthContext);
+  const { token } = useContext(AuthContext);
   const { isAuthenticated } = useContext(AuthContext);
   const [error, setError] = useState("");
   const [showConfirmationPopup, setShowConfirmationPopup] = useState(false);
@@ -42,13 +41,17 @@ const Reel = ({ user, reel, removeReel }) => {
   const handleCancelDelete = () => {
     setShowConfirmationPopup(false);
   };
-
+if (
+    !searchTerm ||
+    reel.text.toLowerCase().includes(searchTerm.toLowerCase())
+  ) {
   return (
     <article className="reel">
       <div className="user-info">
         <img src={`https://source.unsplash.com/random/30x30?sig=${reel.user_id}`} alt="User Profile" />
         <div className="user-email">Usuario: {reel.email}</div>
         <button onClick={handleDeleteReel}>Delete reel</button>
+          <LikeComponent className="buttonLike" reelId={reel.id}/>
       </div>
       <ConfirmationPopup
         show={showConfirmationPopup}
@@ -62,8 +65,6 @@ const Reel = ({ user, reel, removeReel }) => {
           <img src={`${base_URL}/uploads/${reel.image}`} alt={reel.text} />
         )}
         <p>Comment: {reel.text}</p>
-        <LikesComponent reel={reel} />
-        <LikeButton reel={reel} />
         <div className="nav">
           <Link to={`/reel/${reel.id}`}>
             Created at: {new Date(reel.created_at).toLocaleDateString()}
@@ -73,6 +74,8 @@ const Reel = ({ user, reel, removeReel }) => {
       </div>
     </article>
   );
-};
+}else {
+    return null;
+  }
 
 export default Reel;
