a95d461e1eaa8d9571571c76d53098c968211bfe
Jazmin Durand <50839980+durand-jazmin@users.noreply.github.com>
2024-03-12 11:07:59 +0100

    Update LikeComponent.jsx

--- frontend/src/components/LikeComponent.jsx
+++ frontend/src/components/LikeComponent.jsx
@@ -41,7 +41,7 @@ export const LikeComponent = ({ reelId }) => {
 
   return (
      <div>
-    <span className="material-icons"  onClick={handleLike} style={{ color: isLiked ? 'red' : 'lightgrey' }}><span class="material-symbols-outlined">favorite</span></span>
+    <span className="material-icons"  onClick={handleLike} style={{ color: isLiked ? 'red' : 'lightgrey' }}><span >favorite</span></span>
   {showWarning && <p style={{ color: "orange" }}>You are not logged in. Your like may not be saved.</p>}
 </div>
   );
